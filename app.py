6c5df60b2f62a1bf925ee06bf1120a022cd3814b
anhdung911922-blip <anhdung911922@gmail.com>
2026-06-16 11:30:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Tiêu đề ứng dụng
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm")
+st.title("💰 Ứng dụng tính tiền gửi tiết kiệm_PhanThiQuynhTram")
 
 # Nhập dữ liệu
 C = st.number_input(
